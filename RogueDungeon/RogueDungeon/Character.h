5975252ef4a833fd09e7657f76f529891cd8a213
engineer-murataydin <murat_aydin14@hotmail.com>
2014-10-13 14:26:19 +0200

    Kleine aanpassingen

--- RogueDungeon/RogueDungeon/Character.h
+++ RogueDungeon/RogueDungeon/Character.h
@@ -1,8 +1,27 @@
 #pragma once
+
+#include "Item.h"
+#include <vector>
+
+using namespace std;
+
 class Character
 {
 public:
 	Character();
 	~Character();
+
+	// 
+	char naam;
+	int level;
+	int lifepoints;
+	int experience_points;
+	int attack;
+	int defence;
+	int attraction;
+	vector<Item> items;
+
+
+private:
 };
 
---
+++ RogueDungeon/RogueDungeon/Controller.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "Controller.h"
+
+using namespace std;
+
+Controller::Controller()
+{
+}
+
+
+Controller::~ Controller()
+{
+}
+
+void save()
+{
+
+}
+
+void load()
+{
+
+}
